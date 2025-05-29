664775c6de37a88f01928f6588feb8ff8f3033f7
Bnjabour-AbdElhakym <areyoubnjabour@gmail.com>
2025-05-29 22:14:09 +0100

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,9 +43,9 @@ class UserController extends Controller
             $medcin = new Medecin();
             $medcin->nom = $request->nom;
             $medcin->email = $request->email;
-            $medcin->mote_de_passe =Hash::make($request->mote_de_passe);
+            $medcin->mote_de_passe = Hash::make($request->mote_de_passe);
             $medcin->spacialitee = $request->spacialitee;
-            $medcin->ville= $request->ville;
+            $medcin->ville = $request->ville;
             $medcin->diplome = $request->diplome;
             $medcin->photo = $request->photo;
             $medcin->experience = $request->experience;
@@ -53,15 +53,15 @@ class UserController extends Controller
             return redirect()->route('accueil')->with('success', 'message envoyee avec succès');
         }
 
-        if ($request->has('role') && $request->role == 'patient'){
-             $request->validate([
+        if ($request->has('role') && $request->role == 'patient') {
+            $request->validate([
                 'nom' => 'required|string',
                 'email' => 'required|email',
                 'mote_de_passe' => 'required',
                 'ville' => 'required',
-                'date_de_naissance'=>'required'|'date',
-                'genre'=>'required',
-               
+                'date_de_naissance' => 'required' | 'date',
+                'genre' => 'required',
+
 
             ]);
             $patient = new Patient();
@@ -69,18 +69,15 @@ class UserController extends Controller
             $patient->email = $request->email;
             $patient->ville = $request->ville;
             $patient->date_de_naissance = $request->date_de_naissance;
-            $patient->photo =$request->photo;
-            $patient->mote_de_passe =Hash::make($request->mote_de_passe);
-            $patient->genre =$request->genre ;
+            $patient->photo = $request->photo;
+            $patient->mote_de_passe = Hash::make($request->mote_de_passe);
+            $patient->genre = $request->genre;
             $patient->save();
-              return redirect()->route('admin')->with('success', 'Médecin inscrit avec succès !');
-
+            return redirect()->route('admin')->with('success', 'Médecin inscrit avec succès !');
         }
 
 
         return "machi fost if";
-
-    
     }
 
     /**
@@ -88,47 +85,46 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-    $medecins = Medecin::all();
-    return view('admin', compact('medecins'));
+        $medecins = Medecin::all();
+        return view('admin', compact('medecins'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
- 
+
     public function editMedecin($id)
-{
-    $medecin =Medecin::findOrFail($id);
-    return view('admin.editMedecin', compact('medecin'));
-}
+    {
+        $medecin = Medecin::findOrFail($id);
+        return view('admin.editMedecin', compact('medecin'));
+    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-    $medecin =Medecin::findOrFail($id);
-    
-    $request->validate([
-        'nom' => 'required|string',
-        'prenom' => 'required|string',
-        'email' => 'required|email',
-        'horaire' => 'nullable|string',
-        'experience' => 'nullable|string',
-    ]);
+        $medecin = Medecin::findOrFail($id);
 
-    $medecin->update($request->only(['nom', 'prenom', 'email', 'horaires', 'experience']));
+        $request->validate([
+            'nom' => 'required|string',
+            'prenom' => 'required|string',
+            'email' => 'required|email',
+            'horaire' => 'nullable|string',
+            'experience' => 'nullable|string',
+        ]);
 
-    return redirect()->route('admin.liste')->with('success', 'Médecin modifié avec succès.');
+        $medecin->update($request->only(['nom', 'prenom', 'email', 'horaires', 'experience']));
 
+        return redirect()->route('admin.liste')->with('success', 'Médecin modifié avec succès.');
     }
 
-   
-public function destroyMedecin($id){
-    $medecin =Medecin::findOrFail($id);
-    $medecin->delete();
 
-    return redirect()->route('admin.liste')->with('success', 'Médecin supprimé avec succès.');
-}
+    public function destroyMedecin($id)
+    {
+        $medecin = Medecin::findOrFail($id);
+        $medecin->delete();
 
+        return redirect()->route('admin.liste')->with('success', 'Médecin supprimé avec succès.');
+    }
 }
